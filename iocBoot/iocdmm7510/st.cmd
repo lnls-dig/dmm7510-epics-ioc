48ef58a830fa985030f37e678eabaca7167b3570
andrei_pereira <andrei.pereira@lnls.br>
2017-07-28 13:49:19 -0300

    iocBoot/../st.cmd: Add inj efficiency feature
    iocBoot/iocdmm7510/st.cmd: Add .db for injection efficiency calculation, .db for ICT 2, and autosave initialization lines for ICT 2.
    autosave/request_files/autosave_ict2.req: Add request file for ICT 2 autosave.

--- iocBoot/iocdmm7510/st.cmd
+++ iocBoot/iocdmm7510/st.cmd
@@ -14,6 +14,8 @@ cd ${TOP}
 #
 # ####################################################
 
+# ########## Single Device Settings ##########
+
 # DMM7510 IP address -------------------------
 epicsEnvSet DMMADDR "10.0.18.54"
 # --------------------------------------------
@@ -39,10 +41,47 @@ epicsEnvSet PICT "AS-Inj:TI-ICT-1:"
 epicsEnvSet RICT ""
 # --------------------------------------------
 
+# ########## Efficiency App Settings #########
+
+# Second DMM7510 IP address ------------------
+epicsEnvSet DMM2ADDR "10.0.18.10"
+# --------------------------------------------
+
+## Naming Convention for DMM7510 for ICT-2 ---
+# P
+epicsEnvSet PDMM2 "AS-Inj:TI-DMM-2:"
+# R
+epicsEnvSet RDMM2 ""
+# --------------------------------------------
+
+## Naming Convention for ICT-2 ---------------
+# P
+epicsEnvSet PICT2 "AS-Inj:TI-ICT-2:"
+# R
+epicsEnvSet RICT2 ""
+# --------------------------------------------
+
+## Naming Convention for Inj Efficiency PVs --
+# P
+epicsEnvSet PEFF "EFF:"
+# R
+epicsEnvSet REFF "EFF:"
+# --------------------------------------------
+
+## Naming Convention for Eff test PVs --------
+# P
+epicsEnvSet PTEST "TEST:"
+# R
+epicsEnvSet RTEST "TEST:"
+# --------------------------------------------
+
+# ########## Enable / Disable lines ""/"#" ###
+
 # Enable/disable module lines ----------------
 epicsEnvSet DMM_line ""
-epicsEnvSet DCCT_line ""
-epicsEnvSet ICT_line "#"
+epicsEnvSet DCCT_line "#"
+epicsEnvSet ICT_line ""
+epicsEnvSet EFF_line ""
 # --------------------------------------------
 
 # ####################################################
@@ -56,13 +95,21 @@ dmm7510_registerRecordDeviceDriver pdbbase
 asSetFilename("$(TOP)/accessSecurityFile.acf")
 
 ${DMM_line}drvAsynIPPortConfigure("DMMPORT", "${DMMADDR}:5025 TCP",0,0,0)
+${EFF_line}drvAsynIPPortConfigure("DMM2PORT", "${DMM2ADDR}:5025 TCP",0,0,0)
 
 ## Load record instances
 
 # DMM
 ${DMM_line}dbLoadRecords("${TOP}/db/dmm7510.db", "P=${PDMM}, R=${RDMM}, PORT=DMMPORT")
+
+# Single device application
 ${DCCT_line}dbLoadRecords("${TOP}/db/dcct.db", "P=${PDCCT}, R=${RDCCT}, Instrument=${PDMM}${RDMM}")
 ${ICT_line}dbLoadRecords("${TOP}/db/ict.db", "P=${PICT}, R=${RICT}, Instrument=${PDMM}${RDMM}")
+
+# Efficiency application
+${EFF_line}dbLoadRecords("${TOP}/db/dmm7510.db", "P=${PDMM2}, R=${RDMM2}, PORT=DMM2PORT")
+${EFF_line}dbLoadRecords("${TOP}/db/ict.db", "P=${PICT2}, R=${RICT2}, Instrument=${PDMM2}${RDMM2}")
+${EFF_line}dbLoadRecords("${TOP}/db/eff.db", "P=${PEFF}, R=${REFF}, ict-1-prefix=${PICT}${RICT}, ict-2-prefix=${PICT2}${RICT2}")
 
 # Specify save file path
 set_savefile_path("$(TOP)", "autosave")
@@ -75,6 +122,8 @@ set_requestfile_path("$(TOP)", "autosave/request_files")
 ${DCCT_line}set_pass0_restoreFile("autosave_dcct.sav")
 # ICT
 ${ICT_line}set_pass0_restoreFile("autosave_ict.sav")
+# ICT-2
+${EFF_line}set_pass0_restoreFile("autosave_ict2.sav")
 
 # Enable/Disable backup files (0->Disable, 1->Enable)
 save_restoreSet_DatedBackupFiles(0)
@@ -99,4 +148,5 @@ iocInit
 ${DCCT_line}create_triggered_set("autosave_dcct.req", "${PDCCT}${RDCCT}SaveTrg", "P=${PDCCT}, R=${RDCCT}")
 # ICT
 ${ICT_line}create_triggered_set("autosave_ict.req", "${PICT}${RICT}SaveTrg", "P=${PICT}, R=${RICT}")
-
+# ICT-2
+${EFF_line}create_triggered_set("autosave_ict2.req", "${PICT2}${RICT2}SaveTrg", "P=${PICT2}, R=${RICT2}")
