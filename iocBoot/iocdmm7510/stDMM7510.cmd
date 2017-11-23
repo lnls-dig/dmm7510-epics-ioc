674ed06d7f4491cc8609d6dc81f796f9c800a52e
Lucas Russo <lerwys@gmail.com>
2017-11-23 09:59:27 -0200

    iocBoot: use standard names for P/R/IP/PORT

--- iocBoot/iocdmm7510/stDMM7510.cmd
+++ iocBoot/iocdmm7510/stDMM7510.cmd
@@ -11,10 +11,10 @@ dmm7510_registerRecordDeviceDriver pdbbase
 
 asSetFilename("$(TOP)/dmm7510App/Db/accessSecurityFile.acf")
 
-drvAsynIPPortConfigure("DMMPORT", "${DEVICE_IP}:${DEVICE_PORT} TCP",0,0,0)
+drvAsynIPPortConfigure("${PORT}", "${IPADDR}:${IPPORT} TCP",0,0,0)
 
 ## Load record instances
-dbLoadRecords("${TOP}/db/dmm7510.db", "P=${PDMM}, R=${RDMM}, PORT=DMMPORT")
+dbLoadRecords("${TOP}/db/dmm7510.db", "P=${P}, R=${R}, PORT=${PORT}")
 
 < save_restore.cmd
 
@@ -30,4 +30,4 @@ iocInit
 # No sequencer program
 
 # Create manual trigger for Autosave
-create_triggered_set("auto_settings_dcct.req", "${PDMM}${RDMM}SaveTrg", "P=${PDMM}, R=${RDMM}")
+create_triggered_set("auto_settings_dcct.req", "${P}${R}SaveTrg", "P=${P}, R=${R}")
