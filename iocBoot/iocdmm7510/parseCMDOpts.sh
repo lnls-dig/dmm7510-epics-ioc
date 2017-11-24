e39bb981b3e0e58985fcfeafb315b959ecb4213c
Lucas Russo <lerwys@gmail.com>
2017-11-24 17:00:21 -0200

    iocBoot: add RDMM option to parseCMDOpts
    This is important so we can differentiate between
    DCCT/ICT and DMM7510 PVs

--- iocBoot/iocdmm7510/parseCMDOpts.sh
+++ iocBoot/iocdmm7510/parseCMDOpts.sh
@@ -4,11 +4,12 @@ while [ "$#" -gt 0 ]; do
     case "$1" in
         "-P") P="$2" ;;
         "-R") R="$2" ;;
+        "-RDMM") RDMM="$2" ;;
         "-i"|"--ip-addr") IPADDR="$2" ;;
         "-p"|"--ip-port") IPPORT="$2" ;;
         "-d"|"--device-type") DEVICE="$2" ;;
         *) echo "Usage:" >&2
-            echo "  $0 -i IPADDR -p IPPORT -d DEVICE [-P P_VAL] [-R R_VAL]" >&2
+            echo "  $0 -i IPADDR -p IPPORT -d DEVICE [-P P_VAL] [-R R_VAL] [-RDMM RDMM_VAL] " >&2
             echo >&2
             echo " Options:" >&2
             echo "  -i or --ip-addr     Configure IP address to connect to device" >&2
@@ -16,6 +17,7 @@ while [ "$#" -gt 0 ]; do
             echo "  -d or --device      Configure DMM device type [DCCT<number>|ICT<number>|DMM<number>]" >&2
             echo "  -P                  Configure value of \$(P) macro" >&2
             echo "  -R                  Configure value of \$(R) macro" >&2
+            echo "  -RDMM               Configure value of \$(RDMM) macro. Must be different from \$(R)" >&2
             exit 2
     esac
 
