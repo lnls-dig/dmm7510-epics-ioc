e8b91ba392e64983a5c1d31021d36aa648bf6961
andrei_pereira <andrei.pereira@lnls.br>
2018-04-25 18:16:56 -0300

    iocBoot/iocdmm7510/: Fix scripts and add 'set -e' and 'set -u' when possible

--- iocBoot/iocdmm7510/parseCMDOpts.sh
+++ iocBoot/iocdmm7510/parseCMDOpts.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+set -e 
+
 usage () {
     echo "Usage:" >&2
-    echo "  $1 -i IPADDR -p IPPORT -d DEVICE [-P P_VAL] [-R R_VAL] " >&2
+    echo "  $1 -t PROCSERV_TELNET_PORT [-P P_VAL] [-R R_VAL] -i IPADDR -p IPPORT -d DEVICE " >&2
     echo >&2
     echo " Options:" >&2
-    echo "  -i or --ip-addr     Configure IP address to connect to device" >&2
-    echo "  -p or --ip-port     Configure IP port number to connect to device" >&2
-    echo "  -d or --device      Configure DMM device type [DCCT<number>|ICT<number>|DMM<number>]" >&2
+    echo "  -t                  Configure procServ telnet port" >&2
     echo "  -P                  Configure value of \$(P) macro" >&2
     echo "  -R                  Configure value of \$(R) macro" >&2
+    echo "  -i                  Configure IP address to connect to device" >&2
+    echo "  -p                  Configure IP port number to connect to device" >&2
+    echo "  -d                  Configure DMM device type [DCCT|ICT|DMM]" >&2
 }
 
-while getopts ":i:p:d:P:R:" opt; do
+while getopts ":t:P:R:i:p:d:" opt; do
   case $opt in
     t) DMM7510_DEVICE_TELNET_PORT="$OPTARG" ;;
     P) P="$OPTARG" ;;
--- iocBoot/iocdmm7510/runProcServ.sh
+++ iocBoot/iocdmm7510/runProcServ.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+set -e
+set +u
+
 # Parse command-line options
 . ./parseCMDOpts.sh "$@"
 
 # Use defaults if not set
 if [ -z "${DMM7510_DEVICE_TELNET_PORT}" ]; then
-   DMM7510_DEVICE_TELNET_PORT=20000
+   DMM7510_DEVICE_TELNET_PORT="20000"
 fi
 
+if [ -z "${DMM7510_INSTANCE}" ]; then
+   DMM7510_INSTANCE="DMM1"
+fi
+
+set -u
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n DMM7510${DMM7510_INSTANCE} -i ^C^D ${DMM7510_DEVICE_TELNET_PORT} ./runGenericCT.sh "$@"
+/usr/local/bin/procServ -f -n DMM7510_${DMM7510_INSTANCE} -i ^C^D ${DMM7510_DEVICE_TELNET_PORT} ./runGenericCT.sh "$@"
