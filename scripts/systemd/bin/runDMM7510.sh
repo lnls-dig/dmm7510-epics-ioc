492b280b2bd4a8cfe3b757468fa44c3583a78a5d
andrei_pereira <andrei.pereira@lnls.br>
2018-08-31 17:03:54 -0300

    scripts/systemd/bin/runDMM7510.sh: Improve instance name check

--- scripts/systemd/bin/runDMM7510.sh
+++ scripts/systemd/bin/runDMM7510.sh
@@ -7,6 +7,15 @@ if [ -z "$DMM7510_INSTANCE" ]; then
     exit 1
 fi
 
+DMM7510_TYPE=$(echo ${DMM7510_INSTANCE} | grep -Eo "[^0-9]+");
+DMM7510_NUMBER=$(echo ${DMM7510_INSTANCE} | grep -Eo "[0-9]+");
+
+if [ -z "$DMM7510_TYPE" ] || [-z "$DMM7510_NUMBER" ]; then
+    echo "Device instance is invalid. Valid device options are: DCCT, ICT, and DMM." >&2
+    echo "The instance format is: <device type><device index>. Example: DCCT1" >&2
+    exit 5
+fi
+
 export DMM7510_CURRENT_PV_AREA_PREFIX=DMM7510_${DMM7510_INSTANCE}_PV_AREA_PREFIX
 export DMM7510_CURRENT_PV_DEVICE_PREFIX=DMM7510_${DMM7510_INSTANCE}_PV_DEVICE_PREFIX
 export DMM7510_CURRENT_DEVICE_IP=DMM7510_${DMM7510_INSTANCE}_DEVICE_IP
@@ -18,12 +27,6 @@ export DMM7510_PV_DEVICE_PREFIX=${!DMM7510_CURRENT_PV_DEVICE_PREFIX}
 export DMM7510_DEVICE_IP=${!DMM7510_CURRENT_DEVICE_IP}
 export DMM7510_DEVICE_PORT=${!DMM7510_CURRENT_DEVICE_PORT}
 export DMM7510_DEVICE_TELNET_PORT=${!DMM7510_CURRENT_DEVICE_TELNET_PORT}
-
-if [ -z "$DMM7510_INSTANCE" ]; then
-    echo "Device instance is invalid. Valid device options are: DCCT, ICT, and DMM." >&2
-    echo "The instance format is: <device type><device index>. Example: DCCT1" >&2
-    exit 5
-fi
 
 ./runProcServ.sh \
     -t "${DMM7510_DEVICE_TELNET_PORT}" \
